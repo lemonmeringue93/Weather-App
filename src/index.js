f4a338916bb81386417584d4c15ecfc8e7e9b8db
lemonmeringue93 <jobelle.zamora@nhs.net>
2022-04-09 18:10:24 +0100

    added geolocation

--- src/index.js
+++ src/index.js
@@ -31,7 +31,6 @@ let h2 = document.querySelector("h2");
 h2.innerHTML = newDate;
 console.log(new Date());
 
-//change day cards to subsequent days
 let search = document.querySelector("#search");
 let apiKey = "798023fb3d35165272c1fae40ceef0ea";
 search.addEventListener("click", getTemp);
@@ -52,24 +51,22 @@ function showTemperature(response) {
   let temperatureElement = document.querySelector("h3");
   temperatureElement.innerHTML = temperature;
 }
-//change temperature
 
-//change celcius to fahrenheit
-function convertCtoF(event) {
-  event.preventDefault();
-  let F = document.querySelector("#h3");
-  let Fnumber = Number(F);
-  F.innerHTML = Fnumber * 1.8 + 32;
-  console.log(F);
+//get temp of city from geolocation
+function geolocTemp(position) {
+  var lon = position.coords.longitude;
+  var lat = position.coords.latitude;
+  let apiKey = "798023fb3d35165272c1fae40ceef0ea";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showGeotemp);
 }
-let fbutton = document.querySelector("#fahrenheit");
-fbutton.addEventListener("click", convertCtoF);
+navigator.geolocation.getCurrentPosition(geolocTemp);
 
-//change fahrenheit to celcius
-function convertFtoC(event) {
-  event.preventDefault();
-  let tempChange2 = document.querySelector("h3");
-  tempChange2.innerHTML = "19";
+//change temp ONLY when geolocation emoji is clicked
+function showGeotemp(response) {
+  let geoTemp = Math.round(response.data.main.temp);
+  let geoTempelement = document.querySelector("h3");
+  geoTempelement.innerHTML = geoTemp;
 }
-let cbutton = document.querySelector("#celsius");
-cbutton.addEventListener("click", convertFtoC);
+let geoEmoji = document.querySelector("#location");
+geoEmoji.addEventListener("click", geolocTemp);
